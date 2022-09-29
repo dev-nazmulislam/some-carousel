7c543bf2ce16617aab15b419094dda63280409d8
dev-nazmulislam <developer.nazmulislam@gmail.com>
2022-09-29 12:06:14 +0600

    images uploded in imagebb

--- src/Component/CarouselData.js
+++ src/Component/CarouselData.js
@@ -1,39 +1,29 @@
-import image1 from ".././assets/carousel-images-1.jpg";
-import image2 from ".././assets/carousel-images-2.jpg";
-import image3 from ".././assets/carousel-images-3.jpg";
-import image4 from ".././assets/carousel-images-4.jpg";
-import image5 from ".././assets/carousel-images-5.jpg";
-import image6 from ".././assets/carousel-images-6.jpg";
-
-import image7 from ".././assets/multiItem-1.jpg";
-import image8 from ".././assets/multiItem-2.jpg";
-import image9 from ".././assets/multiItem-3.jpg";
-import image10 from ".././assets/multiItem-4.jpg";
-import image11 from ".././assets/multiItem-5.jpg";
-import image12 from ".././assets/multiItem-6.jpg";
-import image13 from ".././assets/multiItem-7.jpg";
-import image14 from ".././assets/multiItem-8.jpg";
-import image15 from ".././assets/multiItem-9.jpg";
-import image16 from ".././assets/multiItem-10.jpg";
-
-import image17 from ".././assets/avatar1.jpg";
-import image18 from ".././assets/avatar2.jpg";
-import image19 from ".././assets/avatar3.jpg";
-import image20 from ".././assets/avatar4.jpg";
-import image21 from ".././assets/avatar5.jpg";
-
-export const CarouselImages = [image1, image2, image3, image4, image5, image6];
-export const MultiImages = [
-  image7,
-  image8,
-  image9,
-  image10,
-  image11,
-  image12,
-  image13,
-  image14,
-  image15,
-  image16,
+export const CarouselImages = [
+  "https://i.ibb.co/W2Z8Tx8/carousel-images-3.jpg",
+  "https://i.ibb.co/68bL5VG/carousel-images-4.jpg",
+  "https://i.ibb.co/f02LxpQ/carousel-images-5.jpg",
+  "https://i.ibb.co/ZSPyFzY/carousel-images-6.jpg",
+  "https://i.ibb.co/vVy3S5H/carousel-images-1.jpg",
+  "https://i.ibb.co/YNRL2rn/carousel-images-2.jpg",
 ];
 
-export const TestimonialImg = [image17, image18, image19, image20, image21];
+export const MultiImages = [
+  "https://i.ibb.co/5jkXkgv/multi-Item-1.webp",
+  "https://i.ibb.co/YLQdTQJ/multi-Item-2.webp",
+  "https://i.ibb.co/pPVHXS2/multi-Item-3.webp",
+  "https://i.ibb.co/pdcr3Nh/multi-Item-4.webp",
+  "https://i.ibb.co/khtBq1t/multi-Item-5.webp",
+
+  "https://i.ibb.co/wphd4cr/multi-Item-6.webp",
+  "https://i.ibb.co/vdG3nfJ/multi-Item-7.webp",
+  "https://i.ibb.co/LNf1WsB/multi-Item-8.webp",
+  "https://i.ibb.co/7Qf3SDv/multi-Item-9.webp",
+];
+
+export const TestimonialImg = [
+  "https://i.ibb.co/47NZR4h/avatar1.png",
+  "https://i.ibb.co/DL8BwLR/avatar2.jpg",
+  "https://i.ibb.co/n7GWzhm/avatar3.webp",
+  "https://i.ibb.co/TMqjXdD/avatar4.webp",
+  "https://i.ibb.co/gZqdNMQ/avatar5.jpg",
+];
